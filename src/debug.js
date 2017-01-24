8105090b3dc2585e3adfd8d8747ba2bcda366a1d
Janay Peters <japeters15@gmail.com>
2017-01-23 22:51:29 -0500

    Utility Tool
    I created a versions function (patch, minor, major)

--- src/debug.js
+++ src/debug.js
@@ -8,6 +8,33 @@ for her Deployment of Web Applications Class at Full Sail University Online
 const colors = require('colors');
 const fs = require('fs');
 
+//bump versions
+function _bump(currentSemVersion, typeofIncrement){
+
+  //Major [0]
+  //Minor [1]
+  //Patch [2]
+  const aryVersions = currentSemVersion.split('.');
+
+  for(let versionIndex in aryVersions){
+    aryVersions[versionIndex] = parseInt(aryVersions[versionIndex]);
+  }
+
+  if(typesofIncrement === 'patch'){
+    aryVersions[2] += 1; //patch
+  } else if(typesofIncrement === 'minor'){
+    aryVersions[2] = 0; //patch
+    aryVersions[1] += 1; //minor
+  } else if(typesofIncrement === 'major'){
+    aryVersions[2] = 0; //patch
+    aryVersions[1] = 0; //minor
+    aryVersions[0] += 1; //minor
+  }
+
+  return aryVersions.join('.');
+}
+
+
 /* Debug Functions */
 function debugSuccess(message) {
   if (process.env.DEBUG) {
